fea8699e847f48a78e1bd01b8caa55377d97f85c
Andrey Pavlov <couchemar@yandex.ru>
2013-01-05 22:28:59 +0400

    Добавили инициализацию задач

--- prepare.go
+++ prepare.go
@@ -5,16 +5,7 @@ import (
 	"log"
 )
 
-func main() {
-	neo, err := neo4j.Connect("http://localhost:7474/db/data")
-	log.Println("Connecting")
-	if err != nil {
-		log.Fatalln("Could not connect to database")
-		return
-	}
-	log.Println("Checking index: users")
-
-	nodes := neo.Nodes
+func initUsers(nodes *neo4j.NodeManager) {
 	indexes := nodes.Indexes
 
 	if _, err := indexes.Get("users"); err == nil {
@@ -46,7 +37,7 @@ func main() {
 
 		_, err = rootNode.Relate("USERS", usersNode.Id(), neo4j.Properties{})
 		if err != nil {
-			log.Fatalln("Could not relate users node to root node")
+			log.Fatalln("Could not relate root node to users node")
 		}
 
 		login := "root"
@@ -58,7 +49,7 @@ func main() {
 		log.Println("Create root user")
 		rootUser, err := nodes.Create(rootUserProps)
 		if err != nil {
-			log.Fatalln("Could not relate root user node")
+			log.Fatalln("Could not create root user node")
 		}
 
 		err = usersIndex.Add(rootUser, "login", login)
@@ -74,4 +65,43 @@ func main() {
 			log.Fatalln("Could not relate root user to users")
 		}
 	}
+}
+
+func initTasks(nodes *neo4j.NodeManager) {
+	rootNode, err := nodes.Get(0)
+
+	if err != nil {
+		log.Fatalln("Failed to get root node")
+	}
+
+	log.Println("Create tasks node")
+	tasksProps := neo4j.Properties{
+		"type": "tasks",
+	}
+	tasksNode, err := nodes.Create(tasksProps)
+
+	if err != nil {
+		log.Fatalln("Could not create tasks node")
+	}
+
+	log.Println("Create relation from root node to tasks node")
+
+	_, err = rootNode.Relate("TASKS", tasksNode.Id(), neo4j.Properties{})
+	if err != nil {
+		log.Fatalln("Could not relate root node to tasks node")
+	}
+}
+
+func main() {
+	neo, err := neo4j.Connect("http://localhost:7474/db/data")
+	log.Println("Connecting")
+	if err != nil {
+		log.Fatalln("Could not connect to database")
+		return
+	}
+	log.Println("Checking index: users")
+
+	nodes := neo.Nodes
+	initUsers(nodes)
+	initTasks(nodes)
 }
