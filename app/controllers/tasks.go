1b36cc63d7484c3c4143b60674bd11268a312fdc
Andrey Pavlov <couchemar@yandex.ru>
2013-01-09 13:30:38 +0400

    Хендлер для добавления таски

--- app/controllers/tasks.go
+++ app/controllers/tasks.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/robfig/revel"
+	"github.com/jmcvetta/neo4j"
 )
 
 type Tasks struct {
@@ -10,4 +11,48 @@ type Tasks struct {
 
 func (c Tasks) Index() rev.Result {
 	return c.Render()
+}
+
+func (c Tasks) CreateTask(summary, description string) rev.Result {
+	neo, err := neo4j.Connect("http://localhost:7474/db/data")
+	nodes := neo.Nodes
+	rootNode, err := nodes.Get(0)
+	if err != nil {
+		rev.ERROR.Fatalln("Failed to get root node")
+		panic(err)
+	}
+
+	/*
+	 Получим узел задач, с ним будут связана вновь созданая задача.
+	 Это map[int] Relation, но фактически там один элемент.
+	 */
+	tasksRels, err := rootNode.Outgoing("TASKS")
+	if err != nil {
+		rev.ERROR.Fatalln("Failed to get TASKS relation")
+		panic(err)
+	}
+
+	// Создадим node задачи.
+	taskProps := neo4j.Properties{
+		"summary": summary,
+		"description": description,
+	}
+	taskNode, err := nodes.Create(taskProps)
+	if err != nil {
+		rev.ERROR.Fatalln("Failed to create task")
+		panic(err)
+	}
+	for _, taskRel := range tasksRels {
+		tasksNode, err := taskRel.End()
+		if err != nil {
+			rev.ERROR.Fatalf("Could not get Tasks Node")
+			panic(err)
+		}
+		_, err = taskNode.Relate("IS_TASK", tasksNode.Id(), neo4j.Properties{})
+		if err != nil {
+			rev.ERROR.Fatalf("Could not relate node %s to tasks node", taskNode)
+			panic(err)
+		}
+	}
+	return c.Redirect(Tasks.Index)
 }
