f4bd1d2002d5204125a67d573c1aaed5342575b9
Andrey Pavlov <couchemar@yandex.ru>
2013-01-10 10:35:07 +0400

    Добавили заглушку для списка задач

---
+++ public/js/tasks.js
@@ -0,0 +1,22 @@
+function Task(data) {
+    this.id = data.Id
+    this.summary = ko.observable(data.Summary);
+    this.description = ko.observable(data.Description);
+}
+
+function TaskListViewModel() {
+    var self = this;
+    self.tasks = ko.observableArray([]);
+
+    // init.
+    $.getJSON("/tasks/list", function(allData) {
+        var tasks = $.map(allData, function(item) {
+            return new Task(item);
+        });
+        self.tasks(tasks);
+    })
+}
+
+$(function() {
+    ko.applyBindings(new TaskListViewModel());
+});
